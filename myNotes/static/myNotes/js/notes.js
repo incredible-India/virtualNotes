38a1fe2b624467772f173e953063cae716d9b489
Himanshu sharma <iamonlyhimanshusharma@gmail.com>
2022-04-17 04:00:45 +0530

    getting to much bug

--- myNotes/static/myNotes/js/notes.js
+++ myNotes/static/myNotes/js/notes.js
@@ -21,7 +21,9 @@ titleNoteTextBox.addEventListener('keyup', function(e) {
 
      title.textContent = titleNoteTextBox.value
      
+   
 
+   
 })
 
 
@@ -73,25 +75,27 @@ colorOftext.onchange = (e)=>{
 
 
 
-let textBoxes = document.getElementsByClassName('b1');
+let textBoxes = document.getElementsByClassName('txtbx');
+
+
 Array.from(textBoxes).forEach((e,i)=>{
 
     
-    e.addEventListener('keyup', function(e){
-
+    e.addEventListener('keyup', function(event){
+//isme hum ctrl aur enter ka func likh sakte hai
 
     var kindOfElement  ;
 
  if (st == 1)
  {
-     kindOfElement = 'h2'
+     kindOfElement = 'h1'
  }
  else if (st == 2)
  {
-     kindOfElement = 'h4'
+     kindOfElement = 'h3'
  }else if(st == 3)
 {
-    kindOfElement = 'h6'
+    kindOfElement = 'h4'
 }
 else if(st == 4)
 {
@@ -101,16 +105,85 @@ else if(st == 4)
     kindOfElement = 'section'
 }
 
-console.log(kindOfElement);
-var createdElement = document.createElement(kindOfElement);
-
-   document.getElementsByClassName('b1d1')[0].appendChild(createdElement);
-        
-     
-        createdElement.textContent = e.target.value;
 
 
-    })
+let Tobe = document.getElementsByClassName(`d${e.classList[1]}`)[0]
+
+Tobe.innerHTML = `<${kindOfElement} class='d${e.classList[1]}t1' > ${e.value}  </${kindOfElement}>`
+ 
+
+//now style through js style
+//all the style codeing can be done from here if any..
+let mkStyle = document.getElementsByClassName(`d${e.classList[1]}t1`)[0]
+//type bold italic or normal
+if (tt == 3)
+{
+
+        mkStyle.style.fontWeight = '600';
+
+}else if (tt == 2)
+{
+
+    mkStyle.style.fontStyle = 'italic';
+
+}else
+{
+    mkStyle.style.fontWeight = '200';
+
+}
+
+
+//color change
+mkStyle.style.color = ct;
+
 
 
 })
+})
+
+
+
+//now clicking on the add button.. 
+
+
+let addBtn =  document.getElementsByClassName('addbtn')[0];
+let insideInsert = document.getElementsByClassName('aldtatoinsert')[0];
+var count =2;
+addBtn.addEventListener('click',(event)=>{
+
+  
+    AddElements(count++);
+})
+
+function AddElements(count){{
+
+    let newDIV = document.createElement('div');
+
+    newDIV.setAttribute('class','input-group container mt-3')
+    newDIV.innerHTML += `
+    
+    <div class="input-group mt-3">
+
+    <div class="container insertdata db${count}">
+  
+    </div>
+
+    <textarea name=""   class="form-control b${count} txtbx" placeholder="Hello World !"></textarea>
+      <span class="input-group-text bi1" id="basic-addon2"><input type="file" name="" id="" class="form-control" placeholder="Add image"></span>
+    </div>
+    
+    
+    `
+    insideInsert.appendChild(newDIV);
+
+}}
+
+
+Array.from(document.getElementsByClassName('txtbx')).forEach(e=>{
+
+    e.addEventListener('click',()=>{
+
+        console.log('isko chhua ',e.className);
+
+    })
+   })
